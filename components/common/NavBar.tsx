a009141e015195abd106d2f856ee73c174b6bd8a
ggsingla <ggsingla777@gmail.com>
2025-04-21 13:04:01 +0530

    update navigation and common components with new design system

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 import { getAllServices } from '@/lib/markdown'
+import { ChevronDownIcon, Bars3Icon } from '@heroicons/react/24/outline'
 
 export type Service = {
   title: string
@@ -16,7 +16,6 @@ export type Service = {
 const NavBar = () => {
   const [mobileNavOpen, setMobileNavOpen] = useState(false)
   const [services, setServices] = useState<Service[]>([])
-  const router = useRouter()
 
   useEffect(() => {
     const getServices = async () => {
@@ -29,26 +28,15 @@ const NavBar = () => {
   return (
     <>
       {/* Main Header Section: With Mega Menu */}
-      <div className='bg-beige-50 font-serif sticky top-0 z-[99999]'>
+      <div className='bg-background font-serif sticky top-0 z-[99999]'>
         {/* Header */}
         <header
           id='page-header'
-          className='flex flex-col flex-none items-center bg-beige-100 py-2'>
+          className='flex flex-col flex-none items-center bg-muted py-2'>
           <div
             onClick={() => setMobileNavOpen(!mobileNavOpen)}
             className='text-center flex gap-5 justify-center items-center w-full py-2 lg:hidden'>
-            <svg
-              className='hi-mini hi-bars-3 inline-block w-5 h-5'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              aria-hidden='true'>
-              <path
-                fillRule='evenodd'
-                d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z'
-                clipRule='evenodd'
-              />
-            </svg>{' '}
+            <Bars3Icon className='w-5 h-5' />
             <p>Menu</p>
           </div>
           <div className='relative container xl:max-w-7xl mx-auto px-4 lg:px-10'>
@@ -58,61 +46,50 @@ const NavBar = () => {
                 <li className='group'>
                   <Link
                     href='/'
-                    className='font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4 text-black'>
+                    className='font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4'>
                     <span>Home</span>
                   </Link>
                 </li>
                 <li className='group'>
                   <Link
                     href='/about-us'
-                    className='font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4 text-black'>
+                    className='font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4'>
                     <span>About Us</span>
                   </Link>
                 </li>
                 <li className='group'>
                   <Link
                     href='/our-ethics'
-                    className='font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4 text-black'>
+                    className='font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4'>
                     <span>Our Ethics</span>
                   </Link>
                 </li>
 
-                <li
-                  onClick={() => {
-                    router.push('/services')
-                  }}
-                  className='group cursor-pointer'>
+                <li className='group'>
                   {/* Services Link */}
-                  <Link
-                    href='/services'
-                    className='cursor-pointer font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4 text-black'>
+                  <div className='font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4'>
                     <span>Services</span>
-                    <svg
-                      fill='currentColor'
-                      viewBox='0 0 20 20'
-                      xmlns='http://www.w3.org/2000/svg'
-                      className='opacity-50 transform transition duration-200 ease-out group-hover:rotate-180 hi-solid hi-chevron-down inline-block w-4 h-4'>
-                      <path
-                        fillRule='evenodd'
-                        d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
-                        clipRule='evenodd'
-                      />
-                    </svg>
-                  </Link>
+                    <ChevronDownIcon className='text-muted-foreground transform transition duration-200 ease-out group-hover:rotate-180 w-4 h-4' />
+                  </div>
 
                   <div className='absolute top-2 pt-8 invisible group-hover:visible z-10'>
-                    <div className='bg-white shadow-xl ring-1 ring-black ring-opacity-5 rounded-lg transform origin-top transition duration-300 ease-out opacity-0 scale-75 group-hover:opacity-100 group-hover:scale-100 overflow-hidden'>
-                      <div className='w-fit transform transition duration-500 ease-out opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100'>
+                    <div className='bg-white shadow-xl ring-1 ring-black ring-opacity-5 transform origin-top transition duration-300 ease-out opacity-0 group-hover:opacity-100 overflow-hidden'>
+                      <div className='w-fit transform transition duration-500 ease-out opacity-0 group-hover:opacity-100'>
                         <div className='p-4 space-y-6'>
                           <nav className='font-sans flex flex-col space-y-3'>
                             {services?.map((service) => (
                               <Link
                                 key={service.slug}
                                 href={`/services/${service.slug}`}
-                                className='text-gray-600 hover:underline underline-offset-4 font-medium text-sm capitalize'>
+                                className='text-foreground hover:underline underline-offset-4 font-medium text-sm capitalize'>
                                 {service.title}
                               </Link>
                             ))}
+                            <Link
+                              href='/services'
+                              className='text-foreground hover:underline underline-offset-4 font-medium text-sm mt-2 pt-2 border-t'>
+                              All Services
+                            </Link>
                           </nav>
                         </div>
                       </div>
@@ -122,52 +99,43 @@ const NavBar = () => {
                 <li className='group'>
                   <Link
                     href='/contact'
-                    className='font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4 text-black'>
+                    className='font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:underline underline-offset-4'>
                     <span>Contact Us</span>
                   </Link>
                 </li>
               </ul>
             </div>
 
-            {/* Navigation on smaller screens */}
             <div className={`lg:hidden ${mobileNavOpen ? '' : 'hidden'}`}>
-              <div className='bg-gray-50 rounded-xl mt-5'>
-                <div className='p-8 space-y-6'>
-                  <nav className='flex flex-col space-y-3'>
-                    <Link
-                      href='/'
-                      className='text-gray-600 hover:text-orange-600 font-medium text-sm'>
-                      <h4 className='font-semibold text-black'>Home</h4>
+              <div className='bg-muted rounded-xl mt-5'>
+                <div className='p-4 space-y-6'>
+                  <nav className='flex flex-col space-y-3 text-foreground'>
+                    <Link href='/' className='font-medium text-sm'>
+                      <h4 className='font-semibold'>Home</h4>
                     </Link>
-                    <Link
-                      href='/about-us'
-                      className='text-gray-600 hover:text-orange-600 font-medium text-sm'>
-                      <h4 className='font-semibold text-black'>About us</h4>
+                    <Link href='/about-us' className='font-medium text-sm'>
+                      <h4 className='font-semibold'>About us</h4>
                     </Link>
-                    <Link
-                      href='/our-ethics'
-                      className='text-gray-600 hover:text-orange-600 font-medium text-sm'>
-                      <h4 className='font-semibold text-black'>Our Ethics</h4>
+                    <Link href='/our-ethics' className='font-medium text-sm'>
+                      <h4 className='font-semibold'>Our Ethics</h4>
                     </Link>
 
-                    <h4 className='text-sm font-semibold text-black'>
+                    <h4 className='text-sm font-semibold'>
                       <Link href='/services'>Services</Link>
                     </h4>
-                    <nav className='flex flex-col space-y-3 pl-3'>
+                    <nav className='flex flex-col space-y-3 pl-4 text-muted-foreground font-light'>
                       {services?.map((service) => (
                         <Link
                           key={service.slug}
                           href={`/services/${service.slug}`}
-                          className='font-sans text-sm text-gray-600 hover:text-orange-600 font-medium capitalize'>
+                          className='font-sans text-sm capitalize'>
                           {service.title}
                         </Link>
                       ))}
                     </nav>
 
-                    <Link
-                      href='/contact'
-                      className='text-gray-600 hover:text-orange-600 font-medium text-sm'>
-                      <h4 className='font-semibold text-black'>Contact Us</h4>
+                    <Link href='/contact' className='font-medium text-sm'>
+                      <h4 className='font-semibold'>Contact Us</h4>
                     </Link>
                   </nav>
                 </div>
